7a72961033350cf11a1ee41b80f7d71eea5fbf07
Mayur Ramesh Kyatham <139049362+Mayur-Kyatham@users.noreply.github.com>
2023-07-27 23:29:51 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,11 @@
+def greeting(name, department):
+    print("Welcome " + name)
+    print("You are part of " + department)
+
+greeting("Mayur", "Data Scientist")
+
+result = greeting("hello", "Mayur")
+print(result)
+
+rest = greeting
+print(rest)
