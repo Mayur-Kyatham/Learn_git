ad6791150588a37883696496519d60ede4306616
Mayur Ramesh Kyatham <139049362+Mayur-Kyatham@users.noreply.github.com>
2023-07-29 23:30:18 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,62 +1,39 @@
-from random import randint
 from art import logo
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
 
-EASY_LEVEL_TURNS = 10
-HARD_LEVEL_TURNS = 5
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
 
-# Function to check user's guess against actual answer.
-
-
-def check_answer(guess, answer, turns):
-    """checks answer against guess. Returns the number of turns remaining."""
-    if guess > answer:
-        print("Too high.")
-        return turns - 1
-    elif guess < answer:
-        print("Too low.")
-        return turns - 1
-    else:
-        print(f"You got it! The answer was {answer}.")
-
-# Make function to set difficulty.
-
-
-def set_difficulty():
-    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
-    if level == "easy":
-        return EASY_LEVEL_TURNS
-    else:
-        return HARD_LEVEL_TURNS
-
-
-def game():
+def calculator():
     print(logo)
-    # Choosing a random number between 1 and 100.
-    print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 100.")
-    answer = randint(1, 100)
-    # print(f"Pssst, the correct answer is {answer}")
 
-    turns = set_difficulty()
-    # Repeat the guessing functionality if they get it wrong.
-    guess = 0
-    while guess != answer:
-        print(f"You have {turns} attempts remaining to guess the number.")
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-        # Let the user guess a number.
-        try:
-            guess = int(input("Make a guess: "))
-        except ValueError:
-            print("Please enter a number.")
-            game()
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-        # Track the number of turns and reduce by 1 if they get it wrong.
-        turns = check_answer(guess, answer, turns)
-        if turns == 0:
-            print("You've run out of guesses, you lose.")
-            return
-        elif guess != answer:
-            print("Guess again.")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
 
-
-game()
+calculator()
