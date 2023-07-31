8fabaf21dcc2f73b119e41c207267902c1085383
Mayur Ramesh Kyatham <139049362+Mayur-Kyatham@users.noreply.github.com>
2023-07-31 17:51:27 +0530

    Update else.py

--- else.py
+++ else.py
@@ -2,4 +2,4 @@ def hint_username(username):
   if len(username) < 3:
     print("Invalid username. Must be atleast 3 characters long")
   else:
-    print(valid username")
+    print("valid username")
