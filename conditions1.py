8783368c88387d98e6696773b3d37bb91b58ef0e
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2023-08-08 21:29:08 +0530

    Create conditions1.py

---
+++ conditions1.py
@@ -0,0 +1,16 @@
+# Use a comparison operator with numbers
+
+# Use a comparison operator to alphabetize strings
+
+
+# The value of 10*4 (40) is greater than 14+23 (37), therefore this 
+# comparison expression will return the Boolean value of True.
+
+
+print(10*4 > 14+23) # Should print True
+
+# The letter "t" has a Unicode value of 116 and the letter "s" has a
+# Unicode value of 115. Since 116 is not less than 115, the 
+# comparison of "tall" < "short" (or 116 < 115) is False. 
+
+print("tall" < "short")  # Should print False
