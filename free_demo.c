3c1216b9d31a4f7c8744dfbaf30b1d6e9103aeaf
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2023-08-26 00:32:35 +0530

    Update free_demo.c

--- free_demo.c
+++ free_demo.c
@@ -4,10 +4,7 @@
 int main(){
     int *ptr;
     int *ptr2;
-    // Sizeof operator in C
-    // printf("The size of int on my pc is %d\n", sizeof(int));
-    // printf("The size of float on my pc is %d\n", sizeof(float));
-    // printf("The size of char on my pc is %d\n", sizeof(char));
+   
     ptr = (int *) malloc(600 * sizeof(int)); 
     for(int i=0; i<600;i++){
         ptr2 = (int *) malloc(600000 * sizeof(int)); 
