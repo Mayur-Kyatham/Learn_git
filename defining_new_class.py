c05fbd88d8e7f297fc2b15254c5d6908106b2938
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2023-09-07 17:39:35 +0530

    Create defining_new_class.py

---
+++ defining_new_class.py
@@ -0,0 +1,20 @@
+class Apple:
+    pass
+
+class Apple:
+    color = ""
+    flavour = ""
+
+mayur = Apple()
+mayur.color = "Red"
+mayur.flavour = "sweet"
+
+print(mayur.color)
+print(mayur.flavour)
+print(mayur.color.upper())
+
+
+golden = Apple()
+golden.color = "yellow"
+golden.flavour = "sour"
+
