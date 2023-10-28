79549c998807b7142a4cf734aa439e20c01e2411
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2023-10-28 22:40:39 +0530

    Update conditions1.py

--- conditions1.py
+++ conditions1.py
@@ -3,6 +3,8 @@
 # Use a comparison operator to alphabetize strings
 
 #Mayur Kyatham 435
+
+#data science
 # The value of 10*4 (40) is greater than 14+23 (37), therefore this 
 # comparison expression will return the Boolean value of True.
 
