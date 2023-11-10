b4a80ab8be922d8b40d84e7e0105b8a890c0458b
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2023-11-11 00:23:54 +0530

    Update writeafunction.py

--- writeafunction.py
+++ writeafunction.py
@@ -2,6 +2,6 @@ def is_leap(year):
     leap = False
     leap = (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0)
     return leap
-// write a function Mayur Kyatham
+// write a function // 
 year = int(input())
 print(is_leap(year))
