92fab71102f7579509b41f084c4b12b2c02ed1e3
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2023-11-15 15:33:34 +0530

    Update list_vs_dictionary.py

--- list_vs_dictionary.py
+++ list_vs_dictionary.py
@@ -3,7 +3,7 @@
 #Here we have a dictionary called "wardrobe" with items of clothing and their colors. 
 #Fill in the blanks to print a line for each item of clothing with each color, 
 #for example: "red shirt", "blue shirt", and so on.
-# Mayur
+# Mayur Kyatham
 #Solution / Answer:
 
 wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
