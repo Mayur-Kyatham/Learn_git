35c85b2ce24c000ddaadbd252cb6418bf6c89833
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2024-01-08 16:30:41 +0530

    Update iterating_dictionary.py

--- iterating_dictionary.py
+++ iterating_dictionary.py
@@ -2,10 +2,8 @@ file_content = {"jpg": 10, "csv": 2, "py": 3, "txt":45}
 for extensions in file_content:
     print(extensions)
 
-
 for ext, amount in file_content.items():
     print("There are {} files with the .{} extension".format(amount, ext))
-
 
 print(file_content.keys())
 
