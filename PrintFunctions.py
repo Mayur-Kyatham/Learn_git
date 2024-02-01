f780b1dc174b9a3b9c1de374308c27ef95cc9050
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2024-02-01 21:09:51 +0530

    Update PrintFunctions.py

--- PrintFunctions.py
+++ PrintFunctions.py
@@ -3,12 +3,9 @@ if __name__ == "__main__":
     ar = range(1, n + 1)
     for i in ar:
         print(i, end="")
-
 """
 Alternate solution:
-
 if __name__ == '__main__':
     n = int(input())
     print("".join([str(i) for i in range(1,n+1)]))
-
 """
