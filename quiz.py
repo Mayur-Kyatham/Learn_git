b8af20d8e181458e37f1cac6b80df88ebda11a0e
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2024-02-15 15:24:32 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -3,3 +3,4 @@ from ipywidgets import interact, Dropdown
 
 question1 = "\033[1mSelect one of the options given:"
 solution1 = {
+'A':'\033[91mNot quite. While both functions are used to create empty arrays, np.zeros() is initialized with the value 0.',
