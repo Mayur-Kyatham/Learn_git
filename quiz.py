8f9ada91de596d61542e52a9df4d3f8ffbb19e16
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2024-02-15 15:24:52 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -5,3 +5,4 @@ question1 = "\033[1mSelect one of the options given:"
 solution1 = {
 'A':'\033[91mNot quite. While both functions are used to create empty arrays, np.zeros() is initialized with the value 0.',
 'B':"\033[91mNot quite. np.zeros() is inialized, and it gives an output of 0's.",
+'C':"\033[91mNot quite. Most often, np.empty() is faster since it is not initialized.",
