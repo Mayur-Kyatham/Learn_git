06326cfbff9de2e5584ba7b47cc8686f7cdad75a
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2024-03-23 15:51:18 +0530

    Create special_method2.py

---
+++ special_method2.py
@@ -0,0 +1,9 @@
+class Apple:
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
+    def __str__(self):
+        return "This apple is {} and its flavor is {}".format(self.color, self.flavor)
+
+mayur = Apple("Red", "Sweet")
+print(mayur)
